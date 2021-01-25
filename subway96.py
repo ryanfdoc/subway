8666adf87b5a9de168a20da80efc6aa474343843
ryanfdoc <ryanfdougherty@gmail.com>
2021-01-25 17:00:28 -0500

    Create subway96.py

---
+++ subway96.py
@@ -0,0 +1,29 @@
+
+# Python Script to Print Next Trains Stopping at 96th Street
+
+import os
+import requests
+import time
+from underground import metadata, SubwayFeed
+
+# Grab API Key from Env file
+# API_KEY = os.getenv('MTA_API_KEY')
+
+# Exaple of SubwayFeed command
+# URL = 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs'
+# feed = SubwayFeed.get(URL)
+
+
+ROUTE1 = '1'
+ROUTE2 = '2'
+ROUTE3 = '3'
+
+Feed1 = underground.SubwayFeed(ROUTE1,api_key='pRNmvpGMmR9lFcp82OIty307RJzAUkFn4seMMYHe')
+Feed2 = underground.SubwayFeed(ROUTE2,api_key='pRNmvpGMmR9lFcp82OIty307RJzAUkFn4seMMYHe')
+Feed3 = underground.SubwayFeed(ROUTE3,api_key='pRNmvpGMmR9lFcp82OIty307RJzAUkFn4seMMYHe')
+
+Feed_ext1 = Feed1.extract_stop_dict()
+Feed_ext2 = Feed2.extract_stop_dict()
+Feed_ext3 = Feed3.extract_stop_dict()
+
+# Hopefully this prints
