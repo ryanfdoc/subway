cf4063dd242c36c3c7f718830fb7f82c382046d4
ryanfdoc <ryanfdougherty@gmail.com>
2021-01-25 20:53:20 -0500

    Update subway96.py

--- src/underground/subway96.py
+++ src/underground/subway96.py
@@ -10,6 +10,8 @@ from underground import metadata, SubwayFeed
 # Grab API Key from Env file
 # API_KEY = os.getenv('MTA_API_KEY')
 
+print("WAR DAMN EAGLE")
+
 # Example of SubwayFeed command
 # URL = 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs'
 # Route = '2'
@@ -20,7 +22,11 @@ from underground import metadata, SubwayFeed
 ROUTE = '1'
 feed = SubwayFeed.get(ROUTE,api_key='pRNmvpGMmR9lFcp82OIty307RJzAUkFn4seMMYHe')
 
-print("Really Hope this Works. WAR EAGLE")
-print(feed.extract_stop_dict())
+Feed_96th = feed.extract_stop_dict()
+
+print(Feed_96th['1']['120S'])
+
+
+
 
 # Hopefully this prints
